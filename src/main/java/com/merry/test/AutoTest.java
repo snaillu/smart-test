ee7d29e217ba94af76b085f843ae830d01549c5a
luqu <luqu@meituan.com>
2018-04-22 22:58:04 +0800

    project init

---
+++ src/main/java/com/merry/test/AutoTest.java
@@ -0,0 +1,12 @@
+package com.merry.test;
+
+/**
+ * @author snail
+ * @create 2018/04/22
+ */
+public class AutoTest {
+
+    public void autoTest(){
+        System.out.println("this is a test");
+    }
+}
---
+++ src/test/java/com/merry/test/AutoTest.java
@@ -0,0 +1,14 @@
+package com.merry.test;
+
+import org.junit.Test;
+
+/**
+ * @author snail
+ * @create 2018/04/22
+ */
+public class AutoTest {
+    @Test
+    public void sayHello(){
+        System.out.println("hello world.");
+    }
+}
